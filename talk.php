e92298b45b15a38d6618f2f0144a011a7c431281
tmastersmart <48128988+tmastersmart@users.noreply.github.com>
2021-09-07 23:50:12 -0500

    Create talk.php

---
+++ talk.php
@@ -0,0 +1,48 @@
+<?php
+//  ------------------------------------------
+//  (c) 2021 by winnfreenet.com all rights recerved 
+//  notice to talk on PI
+//  v1.0
+//  This converts the notice to a wav file 
+//
+//  
+// notes  Install espeak
+// see https://www.dexterindustries.com/howto/make-your-raspberry-pi-speak/
+//
+// aplay /usr/share/sounds/alsa/*
+// sudo apt-get install espeak
+// espeak "Text you wish to hear back" 2>/dev/null
+
+
+
+include "input-scan.php";
+for ($i=0; $i < sizeof($fieldNames); $i++) {
+if ($fieldNames[$i] == 'talk')    {$talk= $fieldValues[$i]; }
+if ($fieldNames[$i] == 'device')  {$device= $fieldValues[$i]; }
+if ($fieldNames[$i] == 'code')    {$code= $fieldValues[$i]; }
+if ($fieldNames[$i] == 'flag')    {$flag= $fieldValues[$i]; }
+}
+
+if (!$talk){$talk="error (no post or get)";}
+
+$log="/home/pi/talk.log";           
+$file="/home/pi/talk.wav"; if(file_exists($file)) { unlink ($file);}
+// set a flag for bash script 
+if ($flag){ 
+   $flagFile="/home/pi/talk-flag.txt";
+   $fileOUT = fopen($flagFile, "w") ;  
+   $fileOUT = fopen($log, "a") ;flock( $fileOUT, LOCK_EX );fwrite ($fileOUT, "Set\n ");flock( $fileOUT, LOCK_UN );fclose ($fileOUT);
+   }          
+
+$send="espeak -w $file $talk";
+//session_write_close();
+exec($send, $output, $return_var ); 
+//session_start();
+$datum = gmdate('[Y-m-d H:i:s]'); // utc_datum
+$status = "$datum : Message:$talk From:$code $device status:$format $return_var";
+print $status;
+
+
+$fileOUT = fopen($log, "a") ;flock( $fileOUT, LOCK_EX );fwrite ($fileOUT, "$status\n ");flock( $fileOUT, LOCK_UN );fclose ($fileOUT);
+
+
