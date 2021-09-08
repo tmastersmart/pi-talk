bdccc441f9be8f185a65cc7adc19e5dba3c9b742
tmastersmart <48128988+tmastersmart@users.noreply.github.com>
2021-09-07 23:59:46 -0500

    Create pi_talk.groovy

---
+++ pi_talk.groovy
@@ -0,0 +1,62 @@
+/**
+Hubitat driver to connect to rasbery pi and talk
+
+ (c) 2021 by tmastersmart winnfreenet.com all rights reserved
+  permission to use on hubiat for free
+ 
+
+you need to get talk.php and talk.sh to run on your pi see this url
+https://github.com/tmastersmart/pi-talk/tree/main
+
+
+
+*   
+* 
+*
+*/
+import java.text.SimpleDateFormat
+metadata {
+    definition (name: "PI Talk no cloud", namespace: "tmastersmart", author: "Tmaster"){
+        capability "Notification"
+        capability "Speech Synthesis"
+ }
+
+                
+    preferences {
+        input("url", "text", title: "URL OF PI:", description: "http://0.0.0.0/talk.php")
+        input("code", "text", title: "Code to identify hub", description: "the hubs name")
+ }              
+}
+
+
+
+
+
+
+
+
+
+def speak(message) {
+    deviceNotification(message)
+}
+
+def deviceNotification(message) {
+
+    
+// what is the varable for the hubs name
+     def params = [
+            uri: "${url}",
+        query: [
+            "talk": "${message}",
+            "code": "${code}",
+        ]
+    ]
+
+   log.info "${device} :Sending Message: ${url}?talk=${message}"    
+
+    httpPost(params){response ->
+            if(response.status != 200) {log.error "${device} :Error ${response.status}. " }
+            else {log.info "${device} :Received ok"}
+    }
+}
+
