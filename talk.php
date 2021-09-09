90919b6f894f1aae78fda94087b11290e60eb05a
tmastersmart <48128988+tmastersmart@users.noreply.github.com>
2021-09-09 16:55:55 -0500

    Update talk.php

--- talk.php
+++ talk.php
@@ -1,23 +1,13 @@
 <?php
-//  ------------------------------------------
+//  ------------------------------------------------------------
 //  (c) 2021 by tmastersmart winnfreenet.com all rights recerved
 //  Permission granted to install and use wuith hubitat for free   
 //
 //   
 //  notice to talk on PI
-//  v1.1 9/08/2021
+//  v2 9/09/2021
 //  
-//  This converts the notice to a wav file 
-//
-//  
-// notes  Install espeak
-// see https://www.dexterindustries.com/howto/make-your-raspberry-pi-speak/
-//
-// aplay /usr/share/sounds/alsa/*
-// sudo apt-get install espeak
-// espeak "Text you wish to hear back" 2>/dev/null
-
-
+//  ------------------------------------------------------------
 
 include "input-scan.php";
 for ($i=0; $i < sizeof($fieldNames); $i++) {
@@ -32,24 +22,26 @@ if (!$lang) {$lang ="-ven-us";} // english us 'espeak --voices' for list
 if (!$voice){$voice="+f1";}
 if (!$talk) {$talk ="error (no post or get)";}
 
-$log="/home/pi/talk.log";           
-$file="/home/pi/talk.wav"; if(file_exists($file)) { unlink ($file);}
-// set a flag for bash script 
-if ($flag){ 
-   $flagFile="/home/pi/talk-flag.txt";
-   $fileOUT = fopen($flagFile, "w") ;  
-   $fileOUT = fopen($log, "a") ;flock( $fileOUT, LOCK_EX );fwrite ($fileOUT, "Set\n ");flock( $fileOUT, LOCK_UN );fclose ($fileOUT);
-   }          
+$log ="/home/pi/talk.log"; 
+$cmd1="/home/pi/talk1.txt"; if(file_exists($cmd1))  { unlink ($cmd1);}
+$cmd2="/home/pi/talk2.txt"; if(file_exists($cmd2))  { unlink ($cmd2);}
 
-$send="espeak $lang$voice -w $file '$talk'";
-//session_write_close();
-exec($send, $output, $return_var ); 
-//session_start();
-$datum = gmdate('[Y-m-d H:i:s]'); // utc_datum
+// v1 wav file
+//$file="/home/pi/talk.wav";  if(file_exists($file))  { unlink ($file);} 
+//$save="$lang$voice '$talk'";
+//$send="espeak $lang$voice -w $file '$talk'";
+//exec($send, $output, $return_var ); 
+
+$datum = date('[Y-m-d H:i:s]'); 
 $status = "$datum : Message:$talk From:$code $device status:$format $return_var";
 print $status;
 
+# build the v2 text command files
+$fileOUT = fopen($cmd1, "w") ;flock( $fileOUT, LOCK_EX );fwrite ($fileOUT, "'$talk'") ;flock( $fileOUT, LOCK_UN );fclose ($fileOUT);
+$fileOUT = fopen($cmd2, "w") ;flock( $fileOUT, LOCK_EX );fwrite ($fileOUT, "$lang$voice") ;flock( $fileOUT, LOCK_UN );fclose ($fileOUT);
 
-$fileOUT = fopen($log, "a") ;flock( $fileOUT, LOCK_EX );fwrite ($fileOUT, "$status\n ");flock( $fileOUT, LOCK_UN );fclose ($fileOUT);
+
+// save the log
+$fileOUT = fopen($log, "a") ;flock( $fileOUT, LOCK_EX );fwrite ($fileOUT, "$status\n");flock( $fileOUT, LOCK_UN );fclose ($fileOUT);
 
 
