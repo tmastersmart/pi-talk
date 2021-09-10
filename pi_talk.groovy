5f17ebb355264b0e4dd7c257725d8a2d1253b0e4
tmastersmart <48128988+tmastersmart@users.noreply.github.com>
2021-09-09 23:36:07 -0500

    Update pi_talk.groovy

--- pi_talk.groovy
+++ pi_talk.groovy
@@ -4,7 +4,7 @@ Hubitat driver to connect to rasbery pi and talk
  (c) 2021 by tmastersmart winnfreenet.com all rights reserved
   permission to use on hubiat for free
 
-
+v1.3  09/09/2021  
 v1.2  09/09/2021  Remove music/added status
 v1.1  09/08/2021
 v1.0  09/08/2021 
@@ -12,9 +12,6 @@ v1.0  09/08/2021
 
 you need to get talk.php and talk.sh to run on your pi see this url
 https://github.com/tmastersmart/pi-talk/tree/main
-
-comments here
-http://www.winnfreenet.com/wp/2021/09/pi-talk-hubitat-to-rasbery-pi-talking-script/
 
 https://raw.githubusercontent.com/tmastersmart/pi-talk/main/pi_talk.groovy
 
@@ -26,10 +23,11 @@ import java.text.SimpleDateFormat
 metadata {
     definition (name: "PI Talk no cloud", namespace: "tmastersmart", author: "Tmaster", importUrl: "https://raw.githubusercontent.com/tmastersmart/pi-talk/main/pi_talk.groovy") {
         capability "Notification"
-       capability "Speech Synthesis"
+	capability "Chime"
+//       capability "Speech Synthesis"
 //        capability "AudioNotification"
 //        capability "MusicPlayer"
-        capability "SpeechSynthesis"
+//        capability "SpeechSynthesis"
        
  }
 
@@ -48,7 +46,31 @@ metadata {
 
 
 
+def playSound(soundnumber){
+// codes are set at the hub    
+     def params = [
+            uri: "${url}",
+        query: [
+            "play": "${soundnumber}",
+            "code": "${code}",
+        ]
+    ]
 
+	
+   log.info "${device} :Sending Chime: ${url}?play=${soundnumber}"    
+   sendEvent(name: "received", value: "${soundnumber}")
+    httpPost(params){response ->
+            if(response.status != 200) {
+                log.error "${device} :Error ${response.status}" 
+                sendEvent(name: "status", value: "Error ${response.status}")
+            }
+            else {
+                log.info "${device} :Received ok"
+                sendEvent(name: "status", value: "ok")
+            }
+        
+    }
+}
 
 
 def speak(message) {
