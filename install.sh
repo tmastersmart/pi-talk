b679593dbc3f0f026e32dc950f092ec6f601cfc9
tmastersmart <48128988+tmastersmart@users.noreply.github.com>
2021-09-09 22:44:31 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#-------------------------------------------------------------
+# (c) 2021 by tmastersmart winnfreenet.com all rights reserved
+#  permission to use on hubiat for free
+
+echo "you should hear talking"
+aplay /usr/share/sounds/alsa/*
+echo "now installing espeak omxplayer"
+sudo apt-get update
+sudo apt-get install espeak
+sudo apt-get install omxplayer
+espeak "Your Install is finished" 2> /dev/null
+
