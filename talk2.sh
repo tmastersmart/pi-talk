728b61a6bab8361eddbb5b6ce5283b7c0974e51a
tmastersmart <48128988+tmastersmart@users.noreply.github.com>
2021-09-11 10:31:22 -0500

    Update talk2.sh

--- talk2.sh
+++ talk2.sh
@@ -5,25 +5,24 @@
 # 
 # Play the talk file v2.0
 # 
-# run with 'watch -n 10 talk.sh'   for looping every 10 sec
+# run with 'watch -n 10 talk2.sh'   for looping every 10 sec
 # launch on start up.
+
 # 
-# Add talk.log to file rotate
-# webmin is recomended to set up log rotate and chron
-# https://www.webmin.com/deb.html
+# place file in /home/pi/
+# place audio files in /home/pi/
+# place audio files in /home/pi/Music/ 
 # 
 # For Hubitat users 
 # -------------------------------------------------------------
 
-#FILE=/home/pi/talk.wav
+PLAY=/home/pi/chime.txt
 MSG1=/home/pi/talk1.txt
 MSG2=/home/pi/talk2.txt
  LOG=/home/pi/talk.log
 DATE=`date +"%Y-%m-%d %T"`
-if [[ ! -f $MSG1 ]] ; then
-    exit
-fi
-
+if [[  -f $MSG1 ]] ; then
+    
  talk=$(cat $MSG1)
  lang=$(cat $MSG2)
  
@@ -35,3 +34,22 @@ fi
  espeak $lang "$talk" 2> /dev/null
  rm $MSG1
  rm $MSG2
+fi
+
+#chime module 
+if [[  -f $PLAY ]] ; then
+
+ chime=$(cat $PLAY)
+ chime=${chime//[$'\t\r\n']}
+#    echo "playing chime $chime"
+    killall omxplayer.bin  2> /dev/null
+    killall omxplayer 2> /dev/null
+    sleep 2
+    nohup /usr/bin/omxplayer --no-keys --no-osd -o local $chime <&- >&- 2>&- & disown
+    echo "[$DATE] : Play file $chime" >> $LOG
+    sleep 10
+    killall omxplayer.bin  2> /dev/null
+    killall omxplayer 2> /dev/null
+    rm $PLAY
+ fi
+
