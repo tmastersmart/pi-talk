eb5de457de14d829a413fc67b3c03f8f22b1ef78
tmastersmart <48128988+tmastersmart@users.noreply.github.com>
2021-09-11 15:25:54 -0500

    Update talk.sh

--- talk.sh
+++ talk.sh
@@ -3,36 +3,55 @@
 # (c) 2021 by tmastersmart winnfreenet.com all rights reserved
 #  permission to use on hubiat for free
 # 
-# Play the talk file v1.1
+# v2.4 9/11/2021 moving files into webserver directory
 # 
-# run with 'watch -n 10 talk.sh'   for looping every 10 sec
+# Play the talk file v2.0
+# 
+# run with 'watch -n 10 talk2.sh'   for looping every 10 sec
 # launch on start up.
+
 # 
-# Add talk.log to file rotate
-# webmin is recomended to set up log rotate and chron
-# https://www.webmin.com/deb.html
+# place file in /home/pi/
+# place audio files in /home/pi/
+# place audio files in /home/pi/Music/ 
 # 
-# For Hubitat users to process the wav file
+# For Hubitat users 
 # -------------------------------------------------------------
 
-FILE=/home/pi/talk.wav
- LOG=/home/pi/talk.log
-DATE=`date +"%Y-%m-%d %T"` 
-if [ -f "$FILE" ]; then
+PLAY=/var/www/html/chime.txt
+MSG1=/var/www/html/talk1.txt
+MSG2=/var/www/html/talk2.txt
+ LOG=/var/www/html/talk.log
+DATE=`date +"%Y-%m-%d %T"`
+if [[  -f $MSG1 ]] ; then
+    
+ talk=$(cat $MSG1)
+ lang=$(cat $MSG2)
+ 
+ talk=${talk//[$'\t\r\n']}
+ lang=${lang//[$'\t\r\n']}
+ 
+ #echo "espeak $lang "$talk" "
+ echo "[$DATE] : Spoke Message $lang "$talk" " >> $LOG
+ espeak $lang "$talk" 2> /dev/null
+ rm $MSG1
+ rm $MSG2
+fi
+
+#chime module 
+if [[  -f $PLAY ]] ; then
+
+ chime=$(cat $PLAY)
+ chime=${chime//[$'\t\r\n']}
+#    echo "playing chime $chime"
     killall omxplayer.bin  2> /dev/null
     killall omxplayer 2> /dev/null
-    sleep 4
-    nohup /usr/bin/omxplayer --no-keys --no-osd -o local /home/pi/talk.wav <&- >&- 2>&- & disown
-    echo "[$DATE] : Spoke Message $lang "$talk" " >> $LOG
+    sleep 2
+    nohup /usr/bin/omxplayer --no-keys --no-osd -o local $chime <&- >&- 2>&- & disown
+    echo "[$DATE] : Play file $chime" >> $LOG
     sleep 10
     killall omxplayer.bin  2> /dev/null
     killall omxplayer 2> /dev/null
-    rm $FILE
-     
-fi
-
-
-
-
-
+    rm $PLAY
+ fi
 
