5ae33ad1245de53f93be54737043d27284896816
tmastersmart <48128988+tmastersmart@users.noreply.github.com>
2021-09-16 00:50:46 -0500

    Update pi_talk.groovy

--- pi_talk.groovy
+++ pi_talk.groovy
@@ -1,12 +1,13 @@
 /**
 PI Talk media GPIO controler
 Hubitat driver to connect to rasbery pi and talk
-capability Notification,Chime,Alarm,MusicPlayer,SpeechSynthesis,switch,buton
+capability Notification,Chime,Alarm,MusicPlayer,SpeechSynthesis,buton,Presence
 ===============================================================
 
 Reads text on the pi or you can play any mp3 file on your pi through pi speakers.
 
-
+v2.1  09-16-2021 Button code changed
+v2.0  09-15-2021 Presence Sensor added. Switch removed due to OFF conflice with Strobe
 v1.9  09/13/2021 Switch and button support added via GPIO (better logs)
 v1.8  09/13/2021 Error Detection added (PI now reports 404 not found for missing mp3)
 v1.7  09/11/2021 PlayTrack support added 
@@ -33,51 +34,98 @@ https://raw.githubusercontent.com/tmastersmart/pi-talk/main/pi_talk.groovy
 import java.text.SimpleDateFormat
 metadata {
     definition (name: "PI Talk media switch controler", namespace: "tmastersmart", author: "WinnFreeNet.com", importUrl: "https://raw.githubusercontent.com/tmastersmart/pi-talk/main/pi_talk.groovy") {
+		capability "Refresh"
         capability "Notification"
 	    capability "Chime"
         capability "Alarm"
-//        capability "AudioNotification"
         capability "MusicPlayer"
         capability "SpeechSynthesis"
         capability "PushableButton"
-        capability "Switch"
+        capability "Presence Sensor"
  }
 
                 
     preferences {
-        input("url", "text", title: "URL OF PI:", description: "http://0.0.0.0/talk.php")
-        input("code", "text", title: "Hubs name", description: "to identify the hub in the pi log")
+        input("url", "text", title: "URL OF PI:", description: "http://0.0.0.0/talk.php",required: true)
+        input("code", "text", title: "Hubs name", description: "to identify the hub in the pi log",required: true)
         input("voice", "text", title: "Voice code", description: "+m1 +m2 +m3 +m4 +m5 +m6 +m7 for male voices and +f1 +f2 +f3 +f4 for female or +croak and +whisper.")
         input("lang", "text", title: "Language", description: "-ven-us USA -ves spanish -vde german(see 'espeak --voices' on pi)")
         input("scode", "text", title: "chime # for siren", description: "Number for siren mp3 file ")
-        input("gpio1", "text", title: "GPIO1 switch", description: "number of GPIO to SWITCH 7 (0 disable)",defaultValue: "0")
-        input("gpio2", "text", title: "GPIO2 button", description: "number of GPIO for Button 10 (0 disable)",defaultValue: "0")
+
+        input("gpio1", "text", title: "1st GPIO", description: "Button 1 on Button 2 OFF. number of GPIO (0 disable)",defaultValue: "0")
+        input("gpio2", "text", title: "2nd GPIO", description: "Button 3 on Button 4 OFF. number of GPIO (0 disable)",defaultValue: "0")
+        input("gpio3", "text", title: "3rd GPIO", description: "Button 5 Press on wait then Off (0 disable)",defaultValue: "0")
+
+        input("url2",  "text", title: "Presence URL", description: "http://0.0.0.0",required: true)
+        input("pollMinutes",  "text", title: "Polling Minutes", description: "Schedule to check",defaultValue: 10,required: true)
+
 
     }              
 }
 
-    
 
 def push(button) {
+    state.gpioSwitch = ""
+    state.gpioState=0
+// button matrix    
+    if (button == 1 ){
+        state.gpio = gpio1 //Button 1 on Button 2 On
+        state.gpioState=1
+        state.gpioSwitch = "1st GPIO ON"
+        customPush(button)
+    }
+    if (button == 2 ){
+        state.gpio = gpio1 //Button 2 on Button 2 OFF
+        state.gpioState=0
+        state.gpioSwitch = "1st GPIO OFF"
+        customPush(button)
+    }
+    if (button == 3 ){
+        state.gpio = gpio2 //Button 3 on Button 4 On
+        state.gpioState=1
+        state.gpioSwitch = "2nd GPIO On"
+        customPush(button)
+    }
+    if (button == 4 ){
+        state.gpio = gpio2 //Button 3 on Button 4 OFF
+        state.gpioState=0
+        state.gpioSwitch = "2nd GPIO OFF"
+        customPush(button)
+    }
+    if (button == 5 ){
+        state.gpio = gpio3 //Button 5 Press on wait then Off
+        state.gpioState=4 
+        state.gpioSwitch = "3rd GPIO Pressed"
+        customPush(button)
+    }
+
+    if (state.gpioSwitch == ""){
+        sendEvent(name: "status", value: "Error")    
+        log.warn "${device} Error Button${button} not supported"
+    }
+       
+}
+
+ def customPush(button){   
        def params = [
             uri: "${url}",
         query: [
-            "gpio": "${gpio2}",
-            "switch": "2",
-            "button": "${button}",
-            "key": "01020304"
+            "gpio": "${state.gpio}",
+            "switch": "${state.gpioState}",
+            "button": "${button}", // we really dont need this. Just for logging
+            "code": "${code}",
         ]
     ]
 	
-   log.info "${device} :Button 1 Pushed GPIO2 ${gpio2}"    
+   log.info "${device} :Button ${button} Pushed GPIO ${state.gpio} State:${state.gpioState}"    
    
 // improved post    
         try {
         httpPost(params) { resp ->
             if (resp.success) {
                log.info "${device} :Received at pi ok"
-               sendEvent(name: "status", value: "ok")
-               sendEvent(name: "pushed", value: "1", isStateChange: true, type: "digital")
+               sendEvent(name: "status", value: "ok ${state.gpioSwitch}")
+               sendEvent(name: "pushed", value: "${button}", isStateChange: true, type: "digital")
             }
             else {log.info "${device} : Received Status ${resp.status}"}
         }
@@ -87,71 +135,14 @@ def push(button) {
     // we dont send a event for switch if error 
     }
 }
-
-
 
 def off(cmd){
-// key is expansion    
-       def params = [
-            uri: "${url}",
-        query: [
-            "gpio": "${gpio1}",
-            "switch": "0",
-            "key": "01020304"
-        ]
-    ]
-	
-   log.info "${device} :OFF GPIO1 ${gpio1}"    
-   
-// improved post    
-        try {
-        httpPost(params) { resp ->
-            if (resp.success) {
-               log.info "${device} :Received at pi ok"
-               sendEvent(name: "status", value: "ok")
-               sendEvent(name: "switch", value: "off")// let the hub know
-            }
-            else {log.info "${device} : Received Status ${resp.status}"}
-        }
-    } catch (Exception e) {
-        sendEvent(name: "status", value: "Error ${e.message}")    
-        log.warn "${device} Error: ${e.message}"
-    // we dont send a event for switch if error 
-    }
-  
+// This does nothing but notify the hub we are off
+// Siren only plays sound then turns off but hub will
+// send off anyway
+  sendEvent(name: "siren", value: "off")
+  sendEvent(name: "strobe", value: "off") 
 }
-
-def on(cmd){
-// key is expansion    
-       def params = [
-            uri: "${url}",
-        query: [
-            "gpio": "${gpio1}",
-            "switch": "1",
-            "key": "01020304"
-        ]
-    ]
-	
-   log.info "${device} :ON GPIO1 ${gpio1}"    
-   
-// improved post    
-        try {
-        httpPost(params) { resp ->
-            if (resp.success) {
-               log.info "${device} :Received at pi ok"
-               sendEvent(name: "status", value: "ok")
-               sendEvent(name: "switch", value: "on")// let the hub know
-            }
-            else {log.info "${device} : Received Status ${resp.status}"}
-        }
-    } catch (Exception e) {
-        sendEvent(name: "status", value: "Error ${e.message}")    
-        log.warn "${device} Error: ${e.message}"
-    // we dont send a event for switch if error 
-    }
-  
-}
-
 
 def siren(cmd){
   playSound(scode)
@@ -276,22 +267,58 @@ def deviceNotification(message) {
         }
     } catch (Exception e) {
         sendEvent(name: "status", value: "Error ${e.message}")    
-        log.warn "${device} Error: ${e.message}"
-  
-    
-    
-    
-    
-//   old way 
-//    httpPost(params){response ->
-//            if(response.status != 200) {
-//                log.error "${device} :Error ${response.status}" 
-//                sendEvent(name: "status", value: "Error ${response.status}")
-//            }
-//            else {
-//                log.info "${device} :Received ok"
-//                sendEvent(name: "status", value: "ok")
-//            }
+        log.warn "${device} Error: ${e.message}" 
     }
+}
+
+
+def updated () {
+	log.debug "${device}: Cron updated ${pollMinutes}min"
+    state.tryPresence = 0
+    schedule("0 */${pollMinutes} * ? * *", refresh)
+    runIn(2, refresh)
+//    sendEvent(name: "presence", value: "present")   
+//    log.info "${device}: Present AT DEFAULT" 
+}
+
+
+
+
+def refresh() {
+    log.info "${device}: refresh"
+	state.tryPresence = state.tryPresence + 1
+    
+    if (state.tryPresence > 3){
+        if (device.currentValue('presence') == "present") {
+         sendEvent(name: "presence", value: "not present")   
+         log.info "${device}: is OFFLINE"   
+        } 
+    }
+    
+    asynchttpGet("httpGetPresence", [uri: url2,timeout: 10]);
+}
+
+
+def httpGetPresence(response, data) {
+	
+	if (response == null || response.class != hubitat.scheduling.AsyncResponse) {
+		return
+	}
+    
+    def st = response.getStatus()
+    
+//    log.debug "${device}: Presence check status =${st}"
+
+	if (st == 200) {
+		state.tryPresence = 0
+		
+		if (device.currentValue('presence') == "not present") {
+           sendEvent(name: "presence", value: "present")   
+           log.info "${device}: is Present"   
+         } 
+//        else {
+//        log.warn "${device}: Presence failed ${state.tryPresence} Tries"
+//        }
+   }
 }
 
