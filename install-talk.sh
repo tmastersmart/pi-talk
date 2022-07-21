5c32364065285cdc16cc4a77a19aac3de1a4c8d8
Tmaster <48128988+tmastersmart@users.noreply.github.com>
2022-07-21 08:38:52 -0500

    Update install-talk.sh

--- install-talk.sh
+++ install-talk.sh
@@ -8,6 +8,7 @@ aplay /usr/share/sounds/alsa/*
 echo "now installing espeak omxplayer gnuplot libraspberrypi-bin"
 sudo apt-get update
 sudo apt-get install espeak
+#  this no longer works in os 11 (sudo apt-get install omxplayer)
 sudo apt-get install omxplayer
 sudo apt-get install gnuplot
 sudo apt-get install libraspberrypi-bin
