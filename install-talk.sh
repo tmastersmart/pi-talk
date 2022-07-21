a506cceeaddf378c911cc7be96820f4cf67c58ea
Tmaster <48128988+tmastersmart@users.noreply.github.com>
2022-07-21 13:30:31 -0500

    Update install-talk.sh

--- install-talk.sh
+++ install-talk.sh
@@ -2,15 +2,27 @@
 #-------------------------------------------------------------
 # (c) 2021 by tmastersmart winnfreenet.com all rights reserved
 #  permission to use on hubiat for free
+
 # Notice OS 11 has major problems unable to install 3 required programs. 
+
+# see https://learn.sparkfun.com/tutorials/raspberry-gpio/all  to instakll GPIO
+git clone https://github.com/WiringPi/WiringPi.git
+cd WiringPi
+git pull origin
+./build
+gpio -v
+
 
 echo "you should hear talking"
 aplay /usr/share/sounds/alsa/*
 echo "now installing espeak omxplayer gnuplot libraspberrypi-bin"
 sudo apt-get update
 sudo apt-get install espeak
+
 #  this no longer works in os 11 (sudo apt-get install omxplayer)
+#  omxplayer will gicve not found. Dont know how to install in os 11
 sudo apt-get install omxplayer
+
 sudo apt-get install gnuplot
 sudo apt-get install libraspberrypi-bin
 sudo apt-get install python3-rpi-gpio
